86688ce9081b683667e89c88e676942806bbfdaa
YiJiuSmile <33725512+YiJiuSmile@users.noreply.github.com>
2019-03-19 17:30:45 +0800

    Update FilePreviewApplication.java

--- jodconverter-web/src/main/java/cn/keking/FilePreviewApplication.java
+++ jodconverter-web/src/main/java/cn/keking/FilePreviewApplication.java
@@ -2,14 +2,21 @@ package cn.keking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(value = "cn.keking.*")
-public class FilePreviewApplication {
+public class FilePreviewApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
         SpringApplication.run(FilePreviewApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        return builder.sources(FilePreviewApplication.class);
+    }
 }
